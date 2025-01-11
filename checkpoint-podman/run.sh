e1d585940df04e6ebe41a66b5fba0daf233647fd
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2025-01-11 09:16:44 -0800

    working podman checkpointing with listening container

---
+++ checkpoint-podman/run.sh
@@ -0,0 +1,37 @@
+# Add CRIU PPA and install
+echo "Adding CRIU PPA and installing..."
+sudo add-apt-repository -y ppa:criu/ppa
+sudo apt-get update
+sudo apt-get install -y criu podman
+
+echo "Building the container"
+buildah build -t serv .
+echo "Running the container"
+podman run -d -p 8080:8080 --name serv serv
+
+echo "Sleeping for 3 seconds"
+sleep 3
+
+echo incrementing the counter
+curl http://localhost:8080
+curl http://localhost:8080
+curl http://localhost:8080
+curl http://localhost:8080
+
+echo checkpointing the container
+podman container checkpoint --export=checkpoint.tar --tcp-established --file-locks serv
+
+echo "Sleeping for 3 seconds"
+sleep 3
+
+echo "Removing the container"
+podman container rm serv
+
+echo "Restoring the container"
+podman container restore --tcp-established --file-locks -p 8080:8080 --import=checkpoint.tar
+
+echo curling
+curl http://localhost:8080
+
+echo removing the checkpoint
+rm -rf checkpoint.tar
