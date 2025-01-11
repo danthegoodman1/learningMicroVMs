e87d8926e59f6ed01c0dbcdd23961cd19d6415c6
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2025-01-11 09:18:57 -0800

    move sleep

--- checkpoint-podman/run.sh
+++ checkpoint-podman/run.sh
@@ -9,8 +9,8 @@ buildah build -t serv .
 echo "Running the container"
 podman run -d -p 8080:8080 --name serv serv
 
-echo "Sleeping for 3 seconds"
-sleep 3
+echo "Sleeping for 1 second"
+sleep 1
 
 echo incrementing the counter
 curl http://localhost:8080
@@ -21,14 +21,14 @@ curl http://localhost:8080
 echo checkpointing the container
 podman container checkpoint --export=checkpoint.tar --tcp-established --file-locks serv
 
-echo "Sleeping for 3 seconds"
-sleep 3
-
 echo "Removing the container"
 podman container rm serv
 
 echo "Restoring the container"
 podman container restore --tcp-established --file-locks -p 8080:8080 --import=checkpoint.tar
+
+echo "Sleeping for 1 second"
+sleep 1
 
 echo curling
 curl http://localhost:8080
